443b4232ab0af197acba17b83481a27e9b591535
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-04-08 21:27:54 +0530

    Update Instahack.py

--- Instahack.py
+++ Instahack.py
@@ -11,3 +11,4 @@ pip install stem
 echo -e "\e[34mSETTING UP SERVERS.....WAIT\e[0m"
 cd $HOME >/dev/null 2>&1
 wget -O ~/instapy-config.json "https://git.io/v5DGy" >/dev/null 2>&1
+echo -e '\e[92mRun instagram-py  --username (your username) --password-list (path of your password list )And run Tor in new section'
