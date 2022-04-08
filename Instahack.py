0e8e8e31878edf4050bddb4f1c2962fa658ce54b
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-04-08 21:15:54 +0530

    Create Instahack.py

---
+++ Instahack.py
@@ -0,0 +1,10 @@
+clear
+echo -e '\e[92m
+             ╦╔═╗  ╦ ╦╔═╗╔═╗╦╔═
+               ║║ ╦──╠═╣╠═╣║  ╠╩╗
+               ╩╚═╝  ╩ ╩╩ ╩╚═╝╩ ╩'  
+apt install -y python2 python tor wget
+pip install --upgrade pip
+pip install requests
+pip install stem
+
