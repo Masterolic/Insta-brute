79ff55eb6fbf481840f0388f15b671ff3a15a01a
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-04-08 21:36:40 +0530

    Update Instahack.py

--- Instahack.py
+++ Instahack.py
@@ -2,7 +2,7 @@ clear
 echo -e '\e[92m
                ╦╔═╗  ╦ ╦╔═╗╔═╗╦╔═
                ║║ ╦──╠═╣╠═╣║  ╠╩╗
-               ╩╚═╝  ╩ ╩╩ ╩╚═╝╩ ╩'  
+               ╩╚═╝  ╩ ╩╩ ╩╚═╝╩ ╩ by @masterolic'  
 apt install -y python2 python tor wget
 pip install --upgrade pip
 pip install requests
@@ -12,3 +12,4 @@ echo -e "\e[34mSETTING UP SERVERS.....WAIT\e[0m"
 cd $HOME >/dev/null 2>&1
 wget -O ~/instapy-config.json "https://git.io/v5DGy" >/dev/null 2>&1
 echo -e '\e[92mRun instagram-py  --username (your username) --password-list (path of your password list )And run Tor in new section'
+echo -e 
