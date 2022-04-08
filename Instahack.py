6dead37dd9864c5d727b7e8fa30e9f437679223c
Masterolic <93469093+Masterolic@users.noreply.github.com>
2022-04-08 21:25:04 +0530

    Update Instahack.py

--- Instahack.py
+++ Instahack.py
@@ -1,6 +1,6 @@
 clear
 echo -e '\e[92m
-             ╦╔═╗  ╦ ╦╔═╗╔═╗╦╔═
+               ╦╔═╗  ╦ ╦╔═╗╔═╗╦╔═
                ║║ ╦──╠═╣╠═╣║  ╠╩╗
                ╩╚═╝  ╩ ╩╩ ╩╚═╝╩ ╩'  
 apt install -y python2 python tor wget
@@ -8,3 +8,6 @@ pip install --upgrade pip
 pip install requests
 pip install stem
 
+echo -e "\e[34mSETTING UP SERVERS.....WAIT\e[0m"
+cd $HOME >/dev/null 2>&1
+wget -O ~/instapy-config.json "https://git.io/v5DGy" >/dev/null 2>&1
